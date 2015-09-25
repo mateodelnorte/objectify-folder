9ced9043fac27093bdfb3f79f74f5a1861a10bf7
Matt Walters <mattwalters5@gmail.com>
2015-09-25 11:07:12 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+var fs = require('fs');
+var path = require('path');
+
+module.exports = function (options) {
+
+  if (typeof options === 'string') {
+    options = {
+      path: options
+    };
+  }
+
+  if ( ! options.path) throw new Error('objectify-folder requires a string dir path or an options param with a path property.');
+
+  if ( ! options.fn) {
+    var index = 0;
+    options.fn = function (mod, result) {
+      result[index++] = mod;
+    };
+  }
+
+  var files = fs.readdirSync(options.path);
+  var result = {};
+
+  files.forEach(function (file) {
+    if (file === 'index.js' || file === '.DS_Store') return;
+    var mod = require(path.resolve(path.join(options.path, file)));
+    options.fn(mod, result);
+  });
+
+  return result;
+
+};
